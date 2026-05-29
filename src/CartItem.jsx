2f717370351f1830e961419ef0ae508087baff6a
Roshni Kushwaha <roshnikushwahaknp101@gmail.com>
2026-05-29 09:49:18 -0400

    updated cartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -24,11 +24,13 @@ const CartItem = ({ onContinueShopping }) => {
 
     const handleContinueShopping = (e) => {
         e.preventDefault();
-
-        onContinueShopping();
+        onContinueShopping(e);
     };
 
+    const handleCheckoutShopping = (e) => {
 
+        alert('Functionality to be added for future reference');
+      };
 
     const handleIncrement = (item) => {
         dispatch(
@@ -88,7 +90,7 @@ const CartItem = ({ onContinueShopping }) => {
             <div className="continue_shopping_btn">
                 <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
                 <br />
-                <button className="get-started-button1">Checkout</button>
+                <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
             </div>
         </div>
     );
